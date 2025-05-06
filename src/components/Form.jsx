38e74a1a933981328afc68d4d3b966df197e3be5
asnaweevixvify <asnaweeezor@gmail.com>
2025-05-06 09:18:18 +0700

    effect

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import './App.css'
 import { v4 as uuidv4 } from 'uuid';
 
 function Form(props){
     const [title,setTitle] = useState('')
     const [amount,setAmount] = useState('')
+    const [formValid,setFormValid] = useState(false)
+    useEffect(()=>{
+        const numericAmount = Number(amount)
+        const checkData = title.trim().length>0 && numericAmount!==0
+        setFormValid(checkData)
+    },[title,amount])
     return(
         <div className='form-container'>
             <form onSubmit={saveItem}>
@@ -12,7 +18,7 @@ function Form(props){
                 <input type='text' placeholder='ระบุชื่อรายการ' onChange={inputTitle} value={title}></input>
                 <label><h2>จำนวนเงิน</h2></label>
                 <input type='number' placeholder='ระบุจำนวนเงิน (รายรับ + , รายจ่าย -)' onChange={inputAmount} value={amount}></input>
-                <button type='submit'>บันทึกรายการ</button>
+                <button type='submit' disabled={!formValid}>บันทึกรายการ</button>
             </form>
         </div>
     )
